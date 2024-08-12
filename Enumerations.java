ec1c9c6a143d1eaf81210a4024d066f76135d59c
nayeem6836 <maseednayeem@gmail.com>
2024-08-12 20:14:02 +0530

    Update Enumerations.java

--- Enumerations.java
+++ Enumerations.java
@@ -1,3 +1,9 @@
+
+Original file line number	Diff line number	Diff line change
+@@ -1,8 +1,38 @@
+import java.util.Stack;
+import java.util.Enumeration;
+
 public class Enumerations{
   public static void main(String[]args){
    // Create an Object for Stack
@@ -6,3 +12,32 @@ public class Enumerations{
   // print the values by using while Loop
 
   }}
+
+    public static void main(String[] args) {
+
+        Stack<Person> s = new Stack<>();
+
+        s.push(new Person("fayaz", 21));
+        s.push(new Person("sai", 21));
+        s.push(new Person("karthik", 21));
+
+        Enumeration<Person> enumeration = s.elements();
+
+        while (enumeration.hasMoreElements()) {
+            System.out.println(enumeration.nextElement());
+        }
+    }
+  }
+    class Person {
+      String name;
+      int age;
+
+      Person(String name, int age) {
+          this.name = name;
+          this.age = age;
+      }
+
+      public String toString() {
+          return name + "  " + age;
+      }
+  }
