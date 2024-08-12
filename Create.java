ee8603d1e541dcb54e33a717a3bbbe93c0786475
nayeem6836 <maseednayeem@gmail.com>
2024-08-12 20:12:16 +0530

    Update Create.java

--- Create.java
+++ Create.java
@@ -1,7 +1,20 @@
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Create{
 public static void main(String[]args){
 // Create an Object for Stack
   // Load Values in Stack
   // Iterate it by usig Iterator and While Loops
+  Stack<Integer> s  = new Stack<>();
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  s.push(4);
+  Iterator<Integer> i = s.iterator();
+  while(i.hasNext()){
+    System.out.println(i.next());
+
+  }
 }
 }
